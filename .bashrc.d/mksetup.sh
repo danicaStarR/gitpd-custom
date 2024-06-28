b755c6568e205219d6c5c2e0f639dac5165b36b6
103302052+danicaStarR@users.noreply.github.com <103302052+danicaStarR@users.noreply.github.com>
2024-06-28 20:42:36 +0800

    Fix

--- .bashrc.d/mksetup.sh
+++ .bashrc.d/mksetup.sh
@@ -1,3 +1,10 @@
 chmod 755 $HOME/setup.sh
 
-$HOME/setup.sh
+# only run once
+if test -e $HOME/.save_gitpod_stat
+then
+  
+else
+  touch $HOME/.save_gitpod_stat
+  $HOME/setup.sh
+fi
--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,7 +1,1 @@
 FROM gitpod/workspace-full-vnc
-
-echo HELLLLLLLO
-
-echo "Welcoem!!" > /etc/motd
-
-USER gitpod
